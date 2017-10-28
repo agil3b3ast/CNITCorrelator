47be562ac4727b95d6869f034d91f13ac058c660
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-10-28 17:15:45 +0200

    Added other plugins to test multi-layer correlation

---
+++ rules/AdvancedLevelCorrelator31.py
@@ -0,0 +1,39 @@
+from preludecorrelator.pluginmanager import Plugin
+from preludecorrelator.idmef import IDMEF
+from preludecorrelator.context import Context
+from preludecorrelator.context import search as context_search
+from preludecorrelator.windows.WeakWindowHelper import WeakWindowHelper
+
+LEVEL = 3
+print("{}, Layer {} Correlation1".format("AdvancedLevelCorrelator", LEVEL))
+context_id = "{}Layer{}Correlation1".format("AdvancedLevelCorrelator", LEVEL)
+
+
+class AdvancedLevelCorrelator31(Plugin):
+
+    def run(self, idmef):
+        corr_name = idmef.get("alert.correlation_alert.name")
+        # We are not interested in simple alerts
+        if corr_name is None:
+         return
+        # We do not want correlation alerts from upper layers
+        if corr_name != "Layer {} Correlation".format(LEVEL - 1):
+         return
+
+        print("{} received correlation".format(self.__class__.__name__))
+        print(corr_name)
+
+        ctx = context_search( context_id)
+        if ctx is None:
+         ctx = Context( context_id, { "expire": 5, "threshold": 2, "window" : 1 ,"alert_on_expire": False }, update = False, idmef=idmef, windowHelper=WeakWindowHelper)
+         ctx.set("alert.correlation_alert.name", "Layer 3 Correlation")
+         ctx.set("alert.classification.text", "MyFirstAdvancedLevelScan31")
+         ctx.set("alert.assessment.impact.severity", "high")
+        else:
+         ctx.update(options={ "expire": 5, "threshold": 2, "window" : 1 ,"alert_on_expire": False }, idmef=idmef)
+
+        if ctx.getWindowHelper().checkCorrelationWindow():
+          print("Hello from %s" % self.__class__.__name__)
+          print(ctx.get("alert.classification.text"))
+          ctx.alert()
+          print("Alert Finished AdvancedLevelCorrelator31")
---
+++ rules/EntryLevelCorrelator12.py
@@ -0,0 +1,35 @@
+from preludecorrelator.pluginmanager import Plugin
+from preludecorrelator.idmef import IDMEF
+from preludecorrelator.context import Context
+from preludecorrelator.context import search as context_search
+from preludecorrelator.windows.WeakWindowHelper import WeakWindowHelper
+
+LEVEL = 1
+print("{}, {} Level Correlation2".format("EntryLevelCorrelator", LEVEL))
+#The context should be unique, it's better add the class name since we know it's unique
+context_id = "{}Layer{}Correlation2".format("EntryLevelCorrelator", LEVEL)
+
+class EntryLevelCorrelator12(Plugin):
+    def run(self, idmef):
+        #Receive only simple alerts, not correlation alerts
+        if idmef.get("alert.correlation_alert.name") is not None:
+         return
+
+        ctx = context_search(context_id)
+        if ctx is None:
+         ctx = Context(context_id, { "expire": 5, "threshold": 5, "window" : 1 ,"alert_on_expire": False }, update = False, idmef=idmef, windowHelper=WeakWindowHelper)
+         #Create a context that:
+         #- expires after 5 seconds of inactivity
+         #- generates a correlation alert after 5 msg received
+         #- checks for the threshold in a window of 1 second, if the window expires the correlation period restarts
+         ctx.set("alert.correlation_alert.name", "Layer {} Correlation".format(LEVEL))
+         ctx.set("alert.classification.text", "MyFirstEntryLevelScan12")
+         ctx.set("alert.assessment.impact.severity", "high")
+        else:
+         ctx.update(options={ "expire": 5, "threshold": 5, "window" : 1 ,"alert_on_expire": False }, idmef=idmef)
+
+        if ctx.getWindowHelper().checkCorrelationWindow():
+          print("Hello from {}".format(self.__class__.__name__))
+          print(ctx.get("alert.classification.text"))
+          ctx.alert()
+          print("{} Alert finished".format(self.__class__.__name__))
