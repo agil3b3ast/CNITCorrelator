e6c3df4fb52e38c2507337aefd9f250600a365de
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-10-29 18:52:29 +0100

    Added simple correlator

---
+++ rules/SimpleCorrelator.py
@@ -0,0 +1,22 @@
+from preludecorrelator.pluginmanager import Plugin
+from preludecorrelator.idmef import IDMEF
+from preludecorrelator.context import Context
+from preludecorrelator.context import search as context_search
+from preludecorrelator.windows.WeakWindowHelper import WeakWindowHelper
+
+
+print("SimpleCorrelator")
+
+class SimpleCorrelator(Plugin):
+    def run(self, idmef):
+         ctx = Context(context_id, { "expire": 5, "threshold": 5, "window" : 1 ,"alert_on_expire": False }, update = True, idmef=idmef)
+         if ctx.getUpdateCount() == 0:
+             ctx.set("alert.correlation_alert.name", "Layer {} Correlation".format(LEVEL))
+             ctx.set("alert.classification.text", "MyFirstEntryLevelScan")
+             ctx.set("alert.assessment.impact.severity", "high")
+
+        if ctx.getUpdateCount() >= ctx.getOptions()["threshold"]:
+          print("Hello from {}".format(self.__class__.__name__))
+          print(ctx.get("alert.classification.text"))
+          ctx.alert()
+          print("{} Alert finished".format(self.__class__.__name__))
