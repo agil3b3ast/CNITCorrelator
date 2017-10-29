60bf42761544aace8fabfea722290a83781a0c7a
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-10-29 19:01:07 +0100

    Changed indentation

--- rules/SimpleCorrelator.py
+++ rules/SimpleCorrelator.py
@@ -9,9 +9,9 @@ class SimpleCorrelator(Plugin):
     def run(self, idmef):
          ctx = Context(context_id, { "expire": 5, "threshold": 5, "window" : 1 ,"alert_on_expire": False }, update = True, idmef=idmef)
          if ctx.getUpdateCount() == 0:
-             ctx.set("alert.correlation_alert.name", "Layer {} Correlation".format(LEVEL))
-             ctx.set("alert.classification.text", "MyFirstEntryLevelScan")
-             ctx.set("alert.assessment.impact.severity", "high")
+          ctx.set("alert.correlation_alert.name", "Layer {} Correlation".format(LEVEL))
+          ctx.set("alert.classification.text", "MyFirstEntryLevelScan")
+          ctx.set("alert.assessment.impact.severity", "high")
 
         if ctx.getUpdateCount() >= ctx.getOptions()["threshold"] - 1:
           print("Hello from {}".format(self.__class__.__name__))
