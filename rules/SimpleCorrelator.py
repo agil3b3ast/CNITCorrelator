7011968216013d202a5d887dcff6426a798e24f0
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-10-29 19:08:16 +0100

    Changed context_id

--- rules/SimpleCorrelator.py
+++ rules/SimpleCorrelator.py
@@ -7,7 +7,7 @@ print("SimpleCorrelator")
 
 class SimpleCorrelator(Plugin):
     def run(self, idmef):
-         ctx = Context(context_id, { "expire": 5, "threshold": 5, "window" : 1 ,"alert_on_expire": False }, update = True, idmef=idmef)
+         ctx = Context("MyContext", { "expire": 5, "threshold": 5, "window" : 1 ,"alert_on_expire": False }, update = True, idmef=idmef)
          if ctx.getUpdateCount() == 0:
           ctx.set("alert.correlation_alert.name", "Layer {} Correlation".format(LEVEL))
           ctx.set("alert.classification.text", "MyFirstEntryLevelScan")
