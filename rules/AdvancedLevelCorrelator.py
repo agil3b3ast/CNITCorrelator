d09da9491fbfe64c040403d60d07050c4676c715
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-10-30 16:12:07 +0100

    Added StrongContextHelper

--- rules/AdvancedLevelCorrelator.py
+++ rules/AdvancedLevelCorrelator.py
@@ -1,11 +1,12 @@
 from preludecorrelator.pluginmanager import Plugin
 from preludecorrelator.idmef import IDMEF
-from preludecorrelator.contexthelpers.WeakContextHelper import WeakContextHelper
+#from preludecorrelator.contexthelpers.WeakContextHelper import WeakContextHelper
+from preludecorrelator.contexthelpers.StrongContextHelper import StrongContextHelper
+from preludecorrelator.context import search as ctx_search
 
 LEVEL = 2
 print("{}, Layer {} Correlation".format("AdvancedLevelCorrelator", LEVEL))
 context_id = "{}Layer{}Correlation".format("AdvancedLevelCorrelator", LEVEL)
-
 
 class AdvancedLevelCorrelator(Plugin):
 
@@ -20,11 +21,16 @@ class AdvancedLevelCorrelator(Plugin):
 
         print("{} received correlation".format(self.__class__.__name__))
 
-        ctx = WeakContextHelper( context_id, { "expire": 1, "threshold": 2 ,"alert_on_expire": False }, update = True, idmef=idmef)
-        if ctx.getUpdateCount() == 0:
+        #ctx = WeakContextHelper( context_id, { "expire": 1, "threshold": 2 ,"alert_on_expire": False }, update = True, idmef=idmef)
+        ctx = ctx_search(context_id)
+        if ctx is None:
+         ctx = StrongContextHelper( context_id, { "expire": 1, "threshold": 2 ,"alert_on_expire": False }, idmef=idmef)
          ctx.set("alert.correlation_alert.name", "Layer 2 Correlation")
          ctx.set("alert.classification.text", "MyFirstAdvancedLevelScan")
          ctx.set("alert.assessment.impact.severity", "high")
+        else:
+         #update context
+         ctx = StrongContextHelper( context_id, { "expire": 1, "threshold": 2 ,"alert_on_expire": False }, idmef=idmef)
 
         if ctx.checkCorrelationAlert():
           print("Hello from %s" % self.__class__.__name__)
--- rules/EntryLevelCorrelator.py
+++ rules/EntryLevelCorrelator.py
@@ -1,6 +1,8 @@
 from preludecorrelator.pluginmanager import Plugin
 from preludecorrelator.idmef import IDMEF
-from preludecorrelator.contexthelpers.WeakContextHelper import WeakContextHelper
+#from preludecorrelator.contexthelpers.WeakContextHelper import WeakContextHelper
+from preludecorrelator.contexthelpers.StrongContextHelper import StrongContextHelper
+from preludecorrelator.context import search as ctx_search
 
 LEVEL = 1
 print("{}, {} Level Correlation".format("EntryLevelCorrelator", LEVEL))
@@ -13,14 +15,19 @@ class EntryLevelCorrelator(Plugin):
         if idmef.get("alert.correlation_alert.name") is not None:
          return
 
-        ctx = WeakContextHelper(context_id, { "expire": 1, "threshold": 5 ,"alert_on_expire": False }, update = True, idmef=idmef)
-        #Create a context that:
-        #- expires after 1 seconds of inactivity
-        #- generates a correlation alert after 5 msg received
-        if ctx.getUpdateCount() == 0:
+        ctx = ctx_search(context_id)
+        if ctx is None:
+         #ctx = WeakContextHelper(context_id, { "expire": 1, "threshold": 5 ,"alert_on_expire": False }, update = True, idmef=idmef)
+         ctx = StrongContextHelper(context_id, { "expire": 1, "threshold": 5 ,"alert_on_expire": False }, idmef=idmef)
+         #Create a context that:
+         #- expires after 1 seconds of inactivity
+         #- generates a correlation alert after 5 msg received
+         #if ctx.getUpdateCount() == 0:
          ctx.set("alert.correlation_alert.name", "Layer {} Correlation".format(LEVEL))
          ctx.set("alert.classification.text", "MyFirstEntryLevelScan")
          ctx.set("alert.assessment.impact.severity", "high")
+        else:
+         ctx = StrongContextHelper(context_id, { "expire": 1, "threshold": 5 ,"alert_on_expire": False }, idmef=idmef)
 
         if ctx.checkCorrelationAlert():
           print("Hello from {}".format(self.__class__.__name__))
