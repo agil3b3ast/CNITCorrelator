b05f3756198a458c7347903141d246487c273bc0
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-10-30 11:53:32 +0100

    Added more plugins to debug

---
+++ rules/AdvancedLevelCorrelator22.py
@@ -0,0 +1,33 @@
+from preludecorrelator.pluginmanager import Plugin
+from preludecorrelator.idmef import IDMEF
+from preludecorrelator.contexthelpers.WeakContextHelper import WeakContextHelper
+
+LEVEL = 2
+print("{}, Layer {} Correlation2".format("AdvancedLevelCorrelator", LEVEL))
+context_id = "{}Layer{}Correlation2".format("AdvancedLevelCorrelator", LEVEL)
+
+
+class AdvancedLevelCorrelator22(Plugin):
+
+    def run(self, idmef):
+        corr_name = idmef.get("alert.correlation_alert.name")
+        # We are not interested in simple alerts
+        if corr_name is None:
+         return
+        # We do not want correlation alerts from upper layers
+        if corr_name != "Layer {} Correlation".format(LEVEL - 1):
+         return
+
+        print("{} received correlation".format(self.__class__.__name__))
+
+        ctx = WeakContextHelper( context_id, { "expire": 1, "threshold": 2 ,"alert_on_expire": False }, update = True, idmef=idmef)
+        if ctx.getUpdateCount() == 0:
+         ctx.set("alert.correlation_alert.name", "Layer 2 Correlation")
+         ctx.set("alert.classification.text", "MyFirstAdvancedLevelScan22")
+         ctx.set("alert.assessment.impact.severity", "high")
+
+        if ctx.checkCorrelationAlert():
+          print("Hello from %s" % self.__class__.__name__)
+          print(ctx.get("alert.classification.text"))
+          ctx.alert()
+          print("Alert Finished AdvancedLevelCorrelator22")
---
+++ rules/EntryLevelCorrelator12.py
@@ -0,0 +1,29 @@
+from preludecorrelator.pluginmanager import Plugin
+from preludecorrelator.idmef import IDMEF
+from preludecorrelator.contexthelpers.WeakContextHelper import WeakContextHelper
+
+LEVEL = 1
+print("{}, {} Level Correlation2".format("EntryLevelCorrelator", LEVEL))
+#The context should be unique, it's better add the class name since we know it's unique
+context_id = "{}Layer{}Correlation2".format("EntryLevelCorrelator", LEVEL)
+
+class EntryLevelCorrelator12(Plugin):
+    def run(self, idmef):
+        #Receive only simple alerts, not correlation alerts
+        if idmef.get("alert.correlation_alert.name") is not None:
+         return
+
+        ctx = WeakContextHelper(context_id, { "expire": 1, "threshold": 5 ,"alert_on_expire": False }, update = True, idmef=idmef)
+        #Create a context that:
+        #- expires after 1 seconds of inactivity
+        #- generates a correlation alert after 5 msg received
+        if ctx.getUpdateCount() == 0:
+         ctx.set("alert.correlation_alert.name", "Layer {} Correlation".format(LEVEL))
+         ctx.set("alert.classification.text", "MyFirstEntryLevelScan12")
+         ctx.set("alert.assessment.impact.severity", "high")
+
+        if ctx.checkCorrelationAlert():
+          print("Hello from {}".format(self.__class__.__name__))
+          print(ctx.get("alert.classification.text"))
+          ctx.alert()
+          print("{} Alert finished".format(self.__class__.__name__))
