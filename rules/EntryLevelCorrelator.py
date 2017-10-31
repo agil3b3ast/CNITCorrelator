9c02e51ba2fb0cb33390f2ec695183ae9932e50b
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-10-31 21:36:34 +0100

    Fixed indentation in plugins

--- rules/EntryLevelCorrelator.py
+++ rules/EntryLevelCorrelator.py
@@ -19,9 +19,7 @@ class EntryLevelCorrelator(Plugin):
         window = self.getWindowHelper(WeakWindowHelper, context_id)
         if window.isEmpty():
          options = { "expire": 1, "threshold": 5 ,"alert_on_expire": False }
-         initial_attrs = {"alert.correlation_alert.name": "Layer {} Correlation".format(LEVEL),
-         "alert.classification.text": "MyFirstEntryLevelScan{}".format(NUMBER),
-         "alert.assessment.impact.severity", "high"}
+         initial_attrs = {"alert.correlation_alert.name": "Layer {} Correlation".format(LEVEL),"alert.classification.text": "MyFirstEntryLevelScan{}".format(NUMBER),"alert.assessment.impact.severity": "high"}
 
          #Create a context that:
          #- expires after 1 seconds of inactivity
