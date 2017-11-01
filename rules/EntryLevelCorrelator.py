f627d535d17f81f00823207eacf10719c03b381e
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-11-01 17:12:32 +0100

    Changed expiration in entry level plugin to 5

--- rules/EntryLevelCorrelator.py
+++ rules/EntryLevelCorrelator.py
@@ -18,7 +18,7 @@ class EntryLevelCorrelator(Plugin):
 
         window = self.getWindowHelper(WeakWindowHelper, context_id)
         if window.isEmpty():
-         options = { "expire": 2, "threshold": 5 ,"alert_on_expire": False }
+         options = { "expire": 5, "threshold": 5 ,"alert_on_expire": False }
          initial_attrs = {"alert.correlation_alert.name": "Layer {} Correlation".format(LEVEL),"alert.classification.text": "MyFirstEntryLevelScan{}".format(NUMBER),"alert.assessment.impact.severity": "high"}
 
          #Create a context that:
