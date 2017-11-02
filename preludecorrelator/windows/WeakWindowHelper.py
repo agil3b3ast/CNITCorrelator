698d54a2631a28ac17095d633a1214abe0527791
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-11-02 12:36:30 +0100

    Changed bindContext in WeakWindowHelper to reset timer after context expiration

--- preludecorrelator/windows/WeakWindowHelper.py
+++ preludecorrelator/windows/WeakWindowHelper.py
@@ -18,12 +18,14 @@ class WeakWindowHelper(WindowHelper):
         res = ctx_search(self._name)
         if res is None:
          self._ctx = Context(self._name, options, update=False)
+         self._origTime = time.time()
         else:
          self._ctx = res
         self._options = options
         self.initialAttrs = initial_attrs
         for key,value in self.initialAttrs.iteritems():
          self._ctx.set(key,value)
+
 
     def _restoreContext(self, options, initial_attrs):
          self._ctx = Context(self._name, options, update=False)
