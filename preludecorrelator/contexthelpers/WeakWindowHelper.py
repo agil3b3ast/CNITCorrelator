a603ec249578270c1c6d66e34f0657dafc6a2f88
Alessandro Fazio <agil3b3ast@hotmail.it>
2017-11-08 19:46:13 +0100

    Added received attr in weak window helper

--- preludecorrelator/contexthelpers/WeakWindowHelper.py
+++ preludecorrelator/contexthelpers/WeakWindowHelper.py
@@ -50,6 +50,7 @@ class WeakWindowHelper(ContextHelper):
         self._received = 0
 
     def processIdmef(self, idmef, addAlertReference=True):
+        self._received = self._received + 1
         now = time.time()
         if now - self._origTime >= self._ctx.getOptions()["window"]:
             if self._ctx.getOptions["reset_ctx_on_window_expiration"]:
@@ -80,7 +81,8 @@ class WeakWindowHelper(ContextHelper):
      return alert_received
     '''
     def countAlertsReceivedInWindow(self):
-        return self._ctx.getUpdateCount()
+        #return self._ctx.getUpdateCount()
+        return self._received
 
     def corrConditions(self):
         alert_received = self.countAlertsReceivedInWindow()
